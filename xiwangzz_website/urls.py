4e850efb25d2c6b2fcaaa6f8794c53a20f4587a2
胡义晋 <jingdiqkl123@qq.com>
2016-11-04 20:28:20 +0800

    website init

---
+++ xiwangzz_website/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import include, url
+from django.views.generic import TemplateView
+
+
+# 首页配置
+urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="website/index.html") , name='index'),
+]
