5eeb4accca81897eb7a79e9c14cdd5904a9cb0db
Eagune <jingdiqkl123@qq.com>
2016-11-07 17:34:51 +0800

    add about page

--- xiwangzz_website/urls.py
+++ xiwangzz_website/urls.py
@@ -5,4 +5,5 @@ from django.views.generic import TemplateView
 # 首页配置
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="website/index.html") , name='index'),
+    url(r'^about/$', TemplateView.as_view(template_name="website/about.html") , name='about'),
 ]
