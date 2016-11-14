e78d9de6b9938ac94eb7f6ee7ac71747e2d13343
胡义晋 <jingdiqkl123@qq.com>
2016-11-14 15:48:23 +0800

    add favicon

--- xiwangzz_website/urls.py
+++ xiwangzz_website/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import include, url
-from django.views.generic import TemplateView
-
+from django.views.generic import TemplateView,RedirectView
+from xiwangzz import settings
 
 # 首页配置
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name="website/index.html") , name='index'),
-    url(r'^about/$', TemplateView.as_view(template_name="website/about.html") , name='about'),
-    url(r'^magzine/$', TemplateView.as_view(template_name="website/magzine.html") , name='magzine'),
-    url(r'^contact/$', TemplateView.as_view(template_name="website/contact.html") , name='contact'),
+    url(r'^$' , TemplateView.as_view(template_name="website/index.html") , name='index'),
+    url(r'^about/$' , TemplateView.as_view(template_name="website/about.html") , name='about'),
+    url(r'^magzine/$' , TemplateView.as_view(template_name="website/magzine.html") , name='magzine'),
+    url(r'^contact/$' , TemplateView.as_view(template_name="website/contact.html") , name='contact'),
+
+    url(r'^favicon\.ico$' , RedirectView.as_view(url=settings.STATIC_URL+'website/favicon.ico',permanent=True) , name='favicon'),
 ]
